af5bd70b20902a1e5d9ed41934dc419c4e584835
Sadan Masroor <sadanmasroor87@gmail.com>
2013-12-18 05:15:43 +0500

    Initial set up of the rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BizzShift::Application.config.secret_key_base = 'bb7ac25b7120cff0450426ea4a7cba3dc96f96d66cd6b63e542824ac45e3613359ea0bed512c7db50664cb9e2e31a1c8cb500a75522c05d6fddc9d02b592991f'
